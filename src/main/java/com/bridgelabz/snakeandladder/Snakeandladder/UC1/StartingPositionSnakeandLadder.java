ed5fb8fb68336949ec44d725cdb8bcb1f3b1209b
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 17:06:35 +0530

    Add:Snake and LAdder game with single player at position 0

---
+++ src/main/java/com/bridgelabz/snakeandladder/Snakeandladder/UC1/StartingPositionSnakeandLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC1;
+
+public class StartingPositionSnakeandLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Game");
+		int position = 0;
+		System.out.println("Position of player is " + position);
+
+	}
+}
---
+++ src/test/java/com/bridgelabz/snakeandladder/Snakeandladder/UC1/SnakeandladderUc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeandladderUc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
