b96271dc709efb603ec18e83cdb016dbad5b073d
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 18:27:51 +0530

    Add:Play the game with 2 Player. In this case if a Player gets a Ladder then plays again.Finally report which Player won the game

---
+++ src/main/java/com/bridgelabz/snakeandladder/Snakeandladder/UC7/SnakeandladderUc7Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeandladderUc7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeandladderUc7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/snakeandladder/Snakeandladder/UC7/SnakeandladderUc7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeandladderUc7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
