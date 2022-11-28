701421a005c15ff4410330876b582764b71250ba
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 18:07:12 +0530

    Add:Player Checks for an Option

---
+++ src/main/java/com/bridgelabz/snakeandladder/Snakeandladder/UC3/SnakeandladderUc3Application.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeandladderUc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeandladderUc3Application.class, args);
+	}
+
+	public void checkingOption() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/snakeandladder/Snakeandladder/UC3/SnakeandladderUc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeandladderUc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
