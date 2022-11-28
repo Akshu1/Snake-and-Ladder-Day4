0ff3496c82cab9d023ad4ad305fb75274dcd468d
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 18:22:06 +0530

    Add:Report the number of times the dice was played to win the game and also the position after every die role

---
+++ src/main/java/com/bridgelabz/snakeandladder/Snakeandladder/UC6/SnakeandladderUc6Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladder.Snakeandladder.UC6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeandladderUc6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeandladderUc6Application.class, args);
+	}
+
+}
